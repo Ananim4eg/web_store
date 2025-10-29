20fb6ece0a522cb4058894bb60114959e5b75a40
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-10-29 10:58:48 +0300

    create new template and model

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from catalog.models import Product
+
 
 def home(request):
     """Контроллер для рендеринга стартовой страницы"""
@@ -8,3 +10,12 @@ def home(request):
 def contacts(request):
     """Контроллер для рендеринга страницы контактов"""
     return render(request, 'contacts.html')
+
+
+def product_info(request, product_id):
+    """Контроллер для рендеринга страницы с информацией о товаре"""
+    product = Product.objects.get(product_id)
+    context = {
+        'product': product
+    }
+    return render(request, 'product_info.html', context=context)
