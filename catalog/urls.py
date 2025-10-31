2d69cff91286962bbd6cbd8d4d0866b2f833335a
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-10-31 11:36:49 +0300

    create new controllers CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_info
+from catalog.views import HomeListView, ContactsView, ProductInfoDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('product_info/<int:product_id>/', products_info, name='product_info')
+    path('', HomeListView.as_view(), name='home_list'),
+    path('contacts/', ContactsView.as_view(), name='contacts_view'),
+    path('product_info/<int:pk>/', ProductInfoDetailView.as_view(), name='product_detail')
 ]
