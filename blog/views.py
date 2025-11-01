f72eec1bbb22459cff5d7052b880a57512144af0
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-11-01 08:23:42 +0300

    create new templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from winreg import DeleteValue
+
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from blog.models import Blog
 
 
@@ -8,3 +10,22 @@ class BlogListView(ListView):
     model = Blog
     template_name = 'blog_main.html'
     context_object_name = 'article'
+
+class BlogCreateView(CreateView):
+    """Контроллер для страницы создания блога"""
+    model = Blog
+    fields = ['title', 'content', 'preview', 'publications_status']
+    template_name = 'blog_article_create.html'
+    success_url = reverse_lazy('blog_main')
+
+class BlogDetailView(DetailView):
+    """Контроллер для страницы с подробной информацией о статье"""
+    model = Blog
+    template_name = 'blog_article_detail.html'
+    context_object_name = 'article'
+
+class BlogUpdateView(UpdateView):
+    pass
+
+class BlogDeleteView(DeleteView):
+    pass
