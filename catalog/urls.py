e578f6a4e15f29df804eb6c764823a80ebd38bc6
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-11-06 10:53:26 +0300

    create 2 controllers and 2 templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import HomeListView, ContactsView, ProductInfoDetailView
+from catalog.views import HomeListView, ContactsView, ProductInfoDetailView, ProductCreateView, ProductUpdateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', HomeListView.as_view(), name='home_list'),
     path('contacts/', ContactsView.as_view(), name='contacts_view'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/update/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
     path('product_info/<int:pk>/', ProductInfoDetailView.as_view(), name='product_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
 from catalog.models import Product
-from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView
 
 
 class HomeListView(ListView):
@@ -20,3 +22,23 @@ class ProductInfoDetailView(DetailView):
     model = Product
     template_name = 'catalog/product_info.html'
     context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    """Контроллер для страницы добавления продукта"""
+    model = Product
+    fields = ['product_name', 'description', 'image', 'category', 'price']
+    template_name = 'catalog/create_product.html'
+    success_url = reverse_lazy('catalog:home_list')
+
+
+class ProductUpdateView(UpdateView):
+    """Контроллер для страницы изменения продукта"""
+    model = Product
+    fields = ['product_name', 'description', 'image', 'category', 'price']
+    template_name = 'catalog/update_product.html'
+    context_object_name = 'product'
+
+    def get_success_url(self):
+        return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
+
