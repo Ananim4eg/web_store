9570cadd970cc72a8a0f0ec8915128fd494ad39a
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-11-06 13:37:59 +0300

    Update style forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -6,10 +6,38 @@ from catalog.models import Product
 
 class ProductForm(forms.ModelForm):
     """Форма для полей модели продуктов"""
+    FORBIDDEN_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                       'радар']
 
     class Meta:
         model = Product
         fields = ['product_name', 'description', 'image', 'category', 'price']
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+
+        self.fields['product_name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите название'
+        })
+
+        self.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите описание товара'
+        })
+
+        self.fields['image'].widget.attrs.update({
+            'class': 'form-control'
+        })
+
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-select'
+        })
+
+        self.fields['price'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите цену'
+        })
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
@@ -18,17 +46,14 @@ class ProductForm(forms.ModelForm):
         return price
 
     def clean(self):
-        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
-                           'радар']
-
         cleaned_data = super().clean()
         product_name = cleaned_data.get('product_name')
         description = cleaned_data.get('description').split()
 
-        if product_name and product_name.lower() in forbidden_words:
+        if product_name and product_name.lower() in self.FORBIDDEN_WORDS:
             self.add_error('product_name', 'Недопустимое название для продукта')
 
         if product_name and description:
             for word in description:
-                if word.lower() in forbidden_words:
+                if word.lower() in self.FORBIDDEN_WORDS:
                     self.add_error('description', f'В описании присутствует недопустимое слово - {word}')
