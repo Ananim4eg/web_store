758b2b67454f3005bb376fc81f6cb91067b7e6c0
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-11-11 00:35:54 +0300

    end release mail notification

--- users/models.py
+++ users/models.py
@@ -5,13 +5,14 @@ from django.db import models
 class CustomUser(AbstractUser):
     """Модель для работы с пользователями"""
 
+    username = None
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='users_avatars/', verbose_name='Аватар', null=True, blank=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     country = models.CharField(max_length=50, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', ]
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
